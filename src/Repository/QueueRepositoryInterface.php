bbf083c99708200cca39e6a2945de918c50b6d39
Samat Abdyldaev <abdyldaevsamat7@gmail.com>
2025-03-14 13:58:30 +0600

    correct repository interface

--- src/Repository/QueueRepositoryInterface.php
+++ src/Repository/QueueRepositoryInterface.php
@@ -6,5 +6,13 @@ use App\Entity\Queue;
 
 interface QueueRepositoryInterface
 {
+    public function addToQueue(string $item): void;
+
     public function getFirstInQueue(): ?Queue;
+
+    public function removeFirstFromQueue(): ?Queue;
+
+    public function getLastInQueue(): ?Queue;
+
+    public function getQueueSize(): int;
 }
